0413875548de57bbc11a210ae5eaa32eb82d8055
sheltonjoseph <shelton013@gmail.com>
2022-09-30 17:59:17 +0530

    update-functionality

--- src/items/items.entity.ts
+++ src/items/items.entity.ts
@@ -1,7 +1,11 @@
 import { Table, Column, Model } from 'sequelize-typescript';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Table
 export class Item extends Model {
+  static save(item: CreateItemDto): import("./dto/create-item.dto").CreateItemDto | PromiseLike<import("./dto/create-item.dto").CreateItemDto> {
+    throw new Error('Method not implemented.');
+  }
   @Column
   description: string;
 
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Inject } from '@nestjs/common';
-
 import { CreateItemDto } from './dto/create-item.dto';
 import { Item } from './items.entity';
 // similar to dto
@@ -20,17 +19,17 @@ export class ItemsService {
   }
 
   async create(item: CreateItemDto): Promise<Item> {
-    const newItem = new this.ItemsRepository(item);
-    const res: any = await newItem.save();
-    return res;
+    // const newItem = new this.ItemsRepository(item);
+    return await this.ItemsRepository.save(item);
   }
 
   async delete(id: number): Promise<any> {
     const res = await this.ItemsRepository.destroy({ where: { id } });
-    console.log(typeof res);
     return res;
   }
   async update(id: number, item: CreateItemDto): Promise<any> {
-    return await this.ItemsRepository.update(item, { where: { id } });
+    // return
+    const result = await this.ItemsRepository.update(item, { where: { id } });
+    return result;
   }
 }
